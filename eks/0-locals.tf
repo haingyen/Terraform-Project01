a1c5ee24d1483c5df75e54806cf3a63d6ba21ee4
26nch <nchb1807838@gmail.com>
2025-04-27 06:39:32 -0700

    add: vpc for eks

---
+++ eks/0-locals.tf
@@ -0,0 +1,8 @@
+locals {
+  env = "staging"
+  region = "ap-southeast-1"
+  az1 = "ap-southeast-1a"
+  az2 = "ap-southeast-1b"
+  eks_name = "demo"
+  eks_version = "1.29"
+}
---
+++ eks/4-subnets.tf
@@ -0,0 +1,50 @@
+resource "aws_subnet" "private_az1" {
+  vpc_id = aws_vpc.main.id
+
+  cidr_block = "10.0.0.0/19"
+  availability_zone = local.az1
+
+  tags = {
+    "Name" = "${local.env}-private-${local.az1}"
+    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/${local.env}-${local.eks_name}" = "owned"
+  }
+}
+
+resource "aws_subnet" "private_az2" {
+  vpc_id = aws_vpc.main.id
+
+  cidr_block = "10.0.32.0/19"
+  availability_zone = local.az2
+
+  tags = {
+    "Name" = "${local.env}-private-${local.az2}"
+    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/${local.env}-${local.eks_name}" = "owned"
+  }
+}
+
+resource "aws_subnet" "public_az1" {
+  vpc_id = aws_vpc.main.id
+
+  cidr_block = "10.0.64.0/19"
+  availability_zone = local.az1
+
+  tags = {
+    "Name" = "${local.env}-public-${local.az1}"
+    "kubernetes.io/role/elb" = "1"
+    "kubernetes.io/cluster/${local.env}-${local.eks_name}" = "owned"
+  }
+}
+resource "aws_subnet" "public_az2" {
+  vpc_id = aws_vpc.main.id
+
+  cidr_block = "10.0.96.0/19"
+  availability_zone = local.az2
+
+  tags = {
+    "Name" = "${local.env}-public-${local.az2}"
+    "kubernetes.io/role/elb" = "1"
+    "kubernetes.io/cluster/${local.env}-${local.eks_name}" = "owned"
+  }
+}
